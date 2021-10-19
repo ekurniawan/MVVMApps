0bef2969c2fda7157499af5ba9eb8794526f832b
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-10-19 15:58:02 +0700

    menambahkan grouping

--- MVVMApps/MVVMApps/ViewModels/CoffeeEquipmentViewModel.cs
+++ MVVMApps/MVVMApps/ViewModels/CoffeeEquipmentViewModel.cs
@@ -3,6 +3,7 @@ using MvvmHelpers;
 using MvvmHelpers.Commands;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
@@ -13,6 +14,7 @@ namespace MVVMApps.ViewModels
     public class CoffeeEquipmentViewModel : ViewModelBase
     {
         public ObservableRangeCollection<Coffee> Coffee { get; set; }
+        public ObservableRangeCollection<Grouping<string,Coffee>> CoffeeGroups { get; set; }
 
         public AsyncCommand RefreshCommand { get; }
 
@@ -20,13 +22,18 @@ namespace MVVMApps.ViewModels
         {
             Title = "List Of Coffee";
             Coffee = new ObservableRangeCollection<Coffee>();
+            CoffeeGroups = new ObservableRangeCollection<Grouping<string, Coffee>>();
+            
             var image = "https://www.yesplz.coffee/app/uploads/2020/11/emptybag-min.png";
 
             Coffee.Add(new Coffee { Roaster = "Dark Roasted", Name = "Italian Roast", Image = image });
             Coffee.Add(new Coffee { Roaster = "Medium Roasted", Name = "Columbia Roast", Image = image });
-            Coffee.Add(new Coffee { Roaster = "Light Roasted", Name = "Toraja Peaberry", Image = image });
+            Coffee.Add(new Coffee { Roaster = "Medium Roasted", Name = "Toraja Peaberry", Image = image });
             Coffee.Add(new Coffee { Roaster = "Dark Roasted", Name = "Tiwus", Image = image });
             Coffee.Add(new Coffee { Roaster = "Medium Roasted", Name = "Pike Market Roast", Image = image });
+
+            CoffeeGroups.Add(new Grouping<string, Coffee>("Medium Roasted",Coffee.Where(c=>c.Roaster== "Medium Roasted")));
+            CoffeeGroups.Add(new Grouping<string, Coffee>("Dark Roasted", Coffee.Where(c=>c.Roaster== "Dark Roasted")));
 
             RefreshCommand = new AsyncCommand(Refresh);
         }

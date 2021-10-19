3bf68b22cb8b4fd46879f3fb5c133eb35ae901b3
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-10-19 13:53:47 +0700

    menambahkan ICommand

--- MVVMApps/MVVMApps/Views/CoffeeEquipmentPage.xaml.cs
+++ MVVMApps/MVVMApps/Views/CoffeeEquipmentPage.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using System.Windows.Input;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -15,9 +15,17 @@ namespace MVVMApps.Views
         public CoffeeEquipmentPage()
         {
             InitializeComponent();
+            IncreaseCount = new Command(OnIncrease);
             BindingContext = this;
         }
 
+        private void OnIncrease()
+        {
+            count++;
+            CountDisplay = $"Anda mengklik {count} kali";
+        }
+
+        public ICommand IncreaseCount { get; }
         int count = 0;
         private string countDisplay= "Click Me"; 
         public string CountDisplay
@@ -31,10 +39,6 @@ namespace MVVMApps.Views
             }
         }   
 
-        private void btnCount_Clicked(object sender, EventArgs e)
-        {
-            count++;
-            CountDisplay = $"Anda mengklik {count} kali";
-        }
+       
     }
 }

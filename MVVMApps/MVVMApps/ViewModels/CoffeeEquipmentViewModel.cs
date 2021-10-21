1eb20d618c39f818d9a5cd163e989c0bfaff000b
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-10-21 09:59:20 +0700

    menambahkan selected command

--- MVVMApps/MVVMApps/ViewModels/CoffeeEquipmentViewModel.cs
+++ MVVMApps/MVVMApps/ViewModels/CoffeeEquipmentViewModel.cs
@@ -14,16 +14,17 @@ namespace MVVMApps.ViewModels
     public class CoffeeEquipmentViewModel : ViewModelBase
     {
         public ObservableRangeCollection<Coffee> Coffee { get; set; }
-        public ObservableRangeCollection<Grouping<string,Coffee>> CoffeeGroups { get; set; }
+        public ObservableRangeCollection<Grouping<string, Coffee>> CoffeeGroups { get; set; }
 
         public AsyncCommand RefreshCommand { get; }
+        public AsyncCommand<Coffee> FavoriteCommand { get; set; }
 
         public CoffeeEquipmentViewModel()
         {
             Title = "List Of Coffee";
             Coffee = new ObservableRangeCollection<Coffee>();
             CoffeeGroups = new ObservableRangeCollection<Grouping<string, Coffee>>();
-            
+
             var image = "https://www.yesplz.coffee/app/uploads/2020/11/emptybag-min.png";
 
             Coffee.Add(new Coffee { Roaster = "Dark Roasted", Name = "Italian Roast", Image = image });
@@ -32,11 +33,39 @@ namespace MVVMApps.ViewModels
             Coffee.Add(new Coffee { Roaster = "Dark Roasted", Name = "Tiwus", Image = image });
             Coffee.Add(new Coffee { Roaster = "Medium Roasted", Name = "Pike Market Roast", Image = image });
 
-            CoffeeGroups.Add(new Grouping<string, Coffee>("Medium Roasted",Coffee.Where(c=>c.Roaster== "Medium Roasted")));
-            CoffeeGroups.Add(new Grouping<string, Coffee>("Dark Roasted", Coffee.Where(c=>c.Roaster== "Dark Roasted")));
+            CoffeeGroups.Add(new Grouping<string, Coffee>("Medium Roasted", Coffee.Where(c => c.Roaster == "Medium Roasted")));
+            CoffeeGroups.Add(new Grouping<string, Coffee>("Dark Roasted", Coffee.Where(c => c.Roaster == "Dark Roasted")));
 
             RefreshCommand = new AsyncCommand(Refresh);
+            FavoriteCommand = new AsyncCommand<Coffee>(Favorite);
         }
+
+        private async Task Favorite(Coffee coffee)
+        {
+            if (coffee == null)
+                return;
+            await Application.Current.MainPage.DisplayAlert("Favorite", coffee.Name, "OK");
+        }
+
+        
+        private Coffee previousSelected;
+        private Coffee selectedCoffee;
+        public Coffee SelectedCoffee
+        {
+            get => selectedCoffee;
+            set
+            {
+                if (value != null)
+                {
+                    Application.Current.MainPage.DisplayAlert("Selected Coffee", value.Name, "OK");
+                    previousSelected = value;
+                    value = null;
+                }
+                selectedCoffee = value;
+                OnPropertyChanged();
+            }
+        }
+
 
         private async Task Refresh()
         {

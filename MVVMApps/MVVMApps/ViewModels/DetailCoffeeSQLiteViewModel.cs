28b2b5ef1068c464ac1375c76c6e5669c50b4722
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-10-22 09:54:15 +0700

    menambahkan detail page

---
+++ MVVMApps/MVVMApps/ViewModels/DetailCoffeeSQLiteViewModel.cs
@@ -0,0 +1,71 @@
+﻿using MVVMApps.Services;
+using MvvmHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace MVVMApps.ViewModels
+{
+    [QueryProperty(nameof(CoffeeId),nameof(CoffeeId))]
+    public class DetailCoffeeSQLiteViewModel : ViewModelBase
+    {
+        public string CoffeeId { get; set; }
+        public AsyncCommand RefreshCommand { get; set; }
+        public AsyncCommand DoneCommand { get; set; }
+
+        public DetailCoffeeSQLiteViewModel()
+        {
+            Title = "Detail Coffee";
+            RefreshCommand = new AsyncCommand(Refresh);
+            DoneCommand = new AsyncCommand(Done);
+        }
+
+        private async Task Done()
+        {
+            await Shell.Current.GoToAsync("..");
+        }
+
+        private async Task Refresh()
+        {
+            int.TryParse(CoffeeId, out var result);
+            var coffee = await CoffeeSQLiteService.GetCofeeById(result);
+
+            Id = coffee.Id;
+            Name = coffee.Name;
+            Roaster = coffee.Roaster;
+            Image = coffee.Image;
+        }
+
+        private int id;
+        public int Id
+        {
+            get { return id; }
+            set { SetProperty(ref id, value); }
+        }
+
+        private string name;
+        public string Name
+        {
+            get { return name; }
+            set { SetProperty(ref name, value); }
+        }
+
+        private string roaster;
+        public string Roaster
+        {
+            get { return roaster; }
+            set { SetProperty(ref roaster, value); }
+        }
+
+        private string image;
+        public string Image
+        {
+            get { return image; }
+            set { SetProperty(ref image, value); }
+        }
+
+
+    }
+}

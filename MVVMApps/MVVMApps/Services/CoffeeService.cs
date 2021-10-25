23abc87610f6bcdb66d384b33896a0e279b4c80a
Erick Kurniawan <erick.kurniawan@gmail.com>
2021-10-25 11:07:32 +0700

    menambahkan remove data

--- MVVMApps/MVVMApps/Services/CoffeeService.cs
+++ MVVMApps/MVVMApps/Services/CoffeeService.cs
@@ -17,19 +17,62 @@ namespace MVVMApps.Services
             _client = new HttpClient();
         }
 
-        public Task AddCoffee(Coffee coffee)
+        public async Task AddCoffee(Coffee coffee)
         {
-            throw new NotImplementedException();
+            var uri = new Uri($"{baseUrl}/api/Coffee");
+            try
+            {
+                var json = JsonConvert.SerializeObject(coffee);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await _client.PostAsync(uri, content);
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception("Gagal menambahkan data coffee");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error: {ex.Message}");
+            }
         }
 
-        public Task EditCoffee(int id, Coffee coffee)
+        public async Task EditCoffee(int id, Coffee coffee)
         {
-            throw new NotImplementedException();
+            var uri = new Uri($"{baseUrl}/api/Coffee{id}");
+            try
+            {
+                var json = JsonConvert.SerializeObject(coffee);
+                var content = new StringContent(json, Encoding.UTF8, "application/json");
+                var response = await _client.PutAsync(uri, content);
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception($"Gagal update data");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error: {ex.Message}");
+            }
         }
 
-        public Task<Coffee> GetCofeeById(int id)
+        public async Task<Coffee> GetCofeeById(int id)
         {
-            throw new NotImplementedException();
+            Coffee coffee = new Coffee();
+            var uri = new Uri($"{baseUrl}/api/Coffee/{id}");
+            try
+            {
+                var response = await _client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    coffee = JsonConvert.DeserializeObject<Coffee>(content);
+                }
+                return coffee;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error: {ex.Message}");
+            }
         }
 
         public async Task<IEnumerable<Coffee>> GetCoffee()
@@ -52,9 +95,21 @@ namespace MVVMApps.Services
             }
         }
 
-        public Task RemoveCoffee(int id)
+        public async Task RemoveCoffee(int id)
         {
-            throw new NotImplementedException();
+            var uri = new Uri($"{baseUrl}/api/Coffee/{id}");
+            try
+            {
+                var response = await _client.DeleteAsync(uri);
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception("Gagal untuk delete data");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new Exception($"Error: {ex.Message}");
+            }
         }
     }
 }
